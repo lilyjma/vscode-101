e8b452efd1585175ae73996b0f8b9321b448a326
Lily Ma <jiayma@microsoft.com>
2019-09-11 11:05:19 -0700

    changed link and uncommented lines

--- test1.py
+++ test1.py
@@ -4,19 +4,20 @@ import sys
 
 import requests
 
-# # print(sys.version)
-# print(sys.executable)
+# print(sys.version)
+print(sys.executable)
 
 
-# def greet(who_to_greet):
-#     greeting = "Hello, {}".format(who_to_greet)
-#     return greeting
+def greet(who_to_greet):
+    greeting = "Hello, {}".format(who_to_greet)
+    return greeting
 
 
-# # print(greet('World'))
-# r = requests.get("http://google.com")
-# print(r.status_code)
+# print(greet('World'))
+r = requests.get("http://microsoft.com")
+print(r.status_code)
+print(r.ok)
 
-name = input("your name? ")
-print("hello,", name)
+# name = input("your name? ")
+# print("hello,", name)
 
