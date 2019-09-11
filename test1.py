51a46587756bdf0fb9e5f99b40faf5cfd4c0e795
Jiayi Ma <jiayma@microsoft.com>
2019-09-11 10:50:16 -0700

    initial commit

---
+++ test1.py
@@ -0,0 +1,22 @@
+import math
+import os
+import sys
+
+import requests
+
+# # print(sys.version)
+# print(sys.executable)
+
+
+# def greet(who_to_greet):
+#     greeting = "Hello, {}".format(who_to_greet)
+#     return greeting
+
+
+# # print(greet('World'))
+# r = requests.get("http://google.com")
+# print(r.status_code)
+
+name = input("your name? ")
+print("hello,", name)
+
